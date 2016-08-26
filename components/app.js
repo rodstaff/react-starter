f4ff2f4c4aa370f6dc68d0e25a088707d4400361
rodk <rod2010rod2010@hotmail.com>
2016-08-26 00:58:08 -0400

    added 'edit/save function'

--- components/app.js
+++ components/app.js
@@ -28,6 +28,7 @@ export default class App extends React.Component {
           <TodosList 
           todos={this.state.todos} 
           toggleTask={this.toggleTask.bind(this)}
+          saveTask={this.saveTask.bind(this)}
         />
       </div>
   	);
@@ -44,6 +45,11 @@ export default class App extends React.Component {
       isCompleted: false
     });
     this.setState({ todos: this.state.todos});
+  }
+  saveTask(oldTask, newTask) {
+    const foundTodo = _.find(this.state.todos, todo => todo.task === oldTask);
+    foundTodo.task = newTask;
+    this.setState({ todos: this.state.todos });
   }
 }
 
--- components/todos-list-item.js
+++ components/todos-list-item.js
@@ -15,6 +15,16 @@ export default class TodosListItem extends React.Component {
       cursor: 'pointer'
     };
 
+    if (this.state.isEditing) {
+      return (
+        <td>
+          <form onSubmit={this.onSaveClick.bind(this)}>
+            <input type="text" defaultValue={task} ref="editInput" />
+          </form>
+        </td>
+      );
+    }
+
     return (
       <td style={taskStyle}
         onClick={this.props.toggleTask.bind(this, task)}
@@ -27,7 +37,7 @@ export default class TodosListItem extends React.Component {
     if (this.state.isEditing) {
       return (
         <td>
-          <button>Save</button>
+          <button onClick={this.onSaveClick.bind(this)}>Save</button>
           <button onClick={this.onCancelClick.bind(this)}>Cancel</button>
         </td>
       );
@@ -50,6 +60,14 @@ export default class TodosListItem extends React.Component {
     this.setState({ isEditing: true});
   }
   onCancelClick() {
+    this.setState({ isEditing: false});
+  }
+  onSaveClick(event) {
+    event.preventDefault();
+
+    const oldTask = this.props.task;
+    const newTask = this.refs.editInput.value;
+    this.props.saveTask(oldTask, newTask);
     this.setState({ isEditing: false});
   }
 }
