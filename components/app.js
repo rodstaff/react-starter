eab174464642bb2e539f40ef3ba99192e4eb0d59
rodk <rod2010rod2010@hotmail.com>
2016-08-26 01:11:45 -0400

    added 'delete function'

--- components/app.js
+++ components/app.js
@@ -29,6 +29,7 @@ export default class App extends React.Component {
           todos={this.state.todos} 
           toggleTask={this.toggleTask.bind(this)}
           saveTask={this.saveTask.bind(this)}
+          deleteTask={this.deleteTask.bind(this)}
         />
       </div>
   	);
@@ -49,6 +50,10 @@ export default class App extends React.Component {
   saveTask(oldTask, newTask) {
     const foundTodo = _.find(this.state.todos, todo => todo.task === oldTask);
     foundTodo.task = newTask;
+    this.setState({ todos: this.state.todos });
+  }
+  deleteTask(taskToDelete) {
+    _.remove(this.state.todos, todo => todo.task === taskToDelete);
     this.setState({ todos: this.state.todos });
   }
 }
