b16c6d1a224ee0560fc9ff2a5d38af8bdc99948d
rodk <rod2010rod2010@hotmail.com>
2016-08-26 23:31:26 -0400

    deleted .babelrc and included in config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,11 @@ module.exports = {
       {
       	test: /\.js$/,
       	exclude: /node_modules/,
-        loader:  'babel-loader'
-        // query: {
-        //   presets: ['react', 'es2015'],
+        loader:  'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
         //   plugins: 'react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
-        // }
+        }
       }
   	]
   },
